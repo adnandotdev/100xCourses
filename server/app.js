7805eb3f01a12ce88ff5be392c495143b7fefbc7
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-11-10 00:52:48 +0530

    qq

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: true, 
+  origin: process.env.FRONTEND_URL, 
   credentials: true,
   methods: ["GET", "POST", "OPTIONS"], 
   allowedHeaders: "Content-Type,Authorization", 

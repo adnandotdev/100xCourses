3a91507ccbaa6b21bc95fd0ead953b6048b1637c
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-11-09 22:38:42 +0530

    updated cors

--- server/app.js
+++ server/app.js
@@ -20,8 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  credentials: true, 
+  origin: process.env.FRONTEND_URL, 
+  credentials: true,
+  methods: "GET,POST", 
+  allowedHeaders: "Content-Type,Authorization", 
 };
 
 app.use(cors(corsOptions));

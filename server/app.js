f9ecda76fc26dd5541a58dd8e5e8b82c3bbf93a2
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-11-10 10:00:49 +0530

    s

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://100x-courses.vercel.app", 
+  origin: "http://100x-courses.vercel.app", 
   credentials: true,
   methods: ["GET", "POST", "OPTIONS"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
